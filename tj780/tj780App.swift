9ebc22a0c01af24ad84b36c2d20c051927102465
mbjemaa <munirbenjemaa@gmail.com>
2024-03-24 16:54:40 -0400

    Initial Commit

---
+++ tj780/tj780App.swift
@@ -0,0 +1,17 @@
+//
+//  tj780App.swift
+//  tj780
+//
+//  Created by Munir Ben Jemaa on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct tj780App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
